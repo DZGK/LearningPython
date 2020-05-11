6c9f7d0eab71963add544d230638a1fd10b6a2ec
dz <drdzmy@gmail.com>
2020-05-11 12:42:01 +0900

    Create sample_tkltk_outlook.py

---
+++ sample_tkltk_outlook.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Tcl/Tk のサンプル
+folderlist から Outlook のフォルダの一覧を読み取り、起動ボタンを表示する。
+ボタンが押されると、指定の Outlook フォルダが開くというスクリプト
+
+cf. http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/event-types.html
+cf. http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/event-modifiers.html
+"""
+import tkinter as tk
+import subprocess
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack(anchor="w", padx="1", pady="1", fill="x")
+        self.create_widgets()
+        self.master.attributes('-topmost', True)
+
+    def create_widgets(self):
+        # フォルダリストからのデータの読み取り
+        linklist = {}
+        with open('folderlist.txt', 'r') as f:
+            read_data = [x.rstrip() for x in f.readlines()]
+
+            for list_item in read_data:
+                k, v = list_item.split(",")
+                linklist[k] = v
+        print(linklist)
+
+        # ファイルを閉じる
+        f.close
+
+        # リンクごとにボタンを生成する
+        for key in linklist:
+            label = key
+            folder = linklist[key]
+
+            self.label = tk.Button(self)
+            self.label["text"] = label
+            self.label.bind("<Enter>", self.change_color_true)
+            self.label.bind("<Leave>", self.change_color_false)
+            self.label["command"] = self.run_it(folder)
+            self.label["anchor"] = "w"
+            self.label.pack(anchor="w", fill="x")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom", anchor="w")
+
+    def run_it(self, folder):
+        def run_outlook():
+            print(folder)
+            subprocess.Popen(r'C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE /select ' + folder)
+
+        return run_outlook
+
+    # ウィジェットにマウスをかざすと色を付ける。
+    def change_color_true(application, event):
+        event.widget["bg"] = "teal"
+        event.widget["fg"] = "white"
+
+    # ウィジェットからマウスが離れると、色を元に戻す
+    def change_color_false(application, event):
+        event.widget["fg"] = "black"
+        event.widget["bg"] = "SystemButtonFace"
+
+    def change_menubar_color_true(application, event):
+        print("chagen_MenuColor")
+        event.widget["bg"] = "teal"
+        event.widget["fg"] = "white"
+
+
+# 処理開始
+root = tk.Tk()
+root.title("Outlookフォルダ")
+app = Application(master=root)
+app.mainloop()
