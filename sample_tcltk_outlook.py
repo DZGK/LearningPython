b7256ec1900233149a47447f11feedd1f91529c1
dz <drdzmy@gmail.com>
2020-05-15 19:59:21 +0900

    Update sample_tcltk_outlook.py

--- sample_tcltk_outlook.py
+++ sample_tcltk_outlook.py
@@ -8,6 +8,9 @@ import tkinter as tk
 import subprocess
 
 
+folderlist_file = "folderlist.txt"
+
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -18,44 +21,53 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         # フォルダリストからのデータの読み取り
-        linklist = {}
-        with open('folderlist.txt', 'r') as f:
+        folderlist = {}
+        with open(folderlist_file, 'r') as f:
             read_data = [x.rstrip() for x in f.readlines()]
 
             for list_item in read_data:
                 k, v = list_item.split(",")
-                linklist[k] = v
-        print(linklist)
+                folderlist[k] = v
 
         # ファイルを閉じる
         f.close
 
-        self.labels =[]
-        # リンクごとにボタンを生成する
-        for key in linklist:
-            label = key
-            folder = linklist[key]
 
-            self.label = tk.Button(self)
-            self.labels.append(self.label)
-            self.label["text"] = label
-            self.label.bind("<Enter>", self.change_color_true)
-            self.label.bind("<Leave>", self.change_color_false)
-            self.label["command"] = self.run_it(folder)
-            self.label["anchor"] = "w"
-            self.label.pack(anchor="w", fill="x")
+#        リンクごとにボタンを生成する。生成したボタンは link_buttons に格納する。
+        self.link_buttons = []
+        for key in folderlist:
+            title = key
+            folder = folderlist[key]
+
+            self.link_button = tk.Button(self)
+            self.link_button["text"] = title
+            self.link_button.bind("<Enter>", self.change_color_true)
+            self.link_button.bind("<Leave>", self.change_color_false)
+            self.link_button["command"] = self.run_it(folder)
+            self.link_button["anchor"] = "w"
+            self.link_button.pack(anchor="w", fill="x")
+
+            # Button ウェジットのおbジュジェクトをを操作用の配列に格納する。
+            self.link_buttons.append(self.link_button)
+
+#       UPDATE ボタンの生成
+        self.update = tk.Button(self, text="UPDATE", fg="blue",
+                                command=self.update_folderlist)
+        self.update.pack(anchor="w")
+        self.link_buttons.append(self.update)
 
 
-        self.quit = tk.Button(self, text="UPDATE", fg="blue",
-                              command=self.update_list)
-        self.quit.pack(anchor="w")
+#       open list file ボタンの生成
+        self.open_listfile = tk.Button(self, text="Open List File", fg="blue",
+                                       command=self.open_folderlist)
+        self.open_listfile.pack(anchor="w")
+        self.link_buttons.append(self.open_listfile)
 
-
+#       QUIT ボタンの生成
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom", anchor="w")
-          
-        
+        self.link_buttons.append(self.quit)
 
     def run_it(self, folder):
         def run_outlook():
@@ -79,50 +91,21 @@ class Application(tk.Frame):
         event.widget["bg"] = "teal"
         event.widget["fg"] = "white"
 
+    def open_folderlist(self):
+        print("open_folderlistfile")
+        subprocess.Popen(r'C:\Windows\system32\notepad.exe ' + folderlist_file)
 
-    def update_list(self):
-      #  print(self.label["text"])
-        print(self.labels)
-        self.labels[3]["text"] = "ヘムレンさん"
-#        for i in self.label:
-#            print(self.i["text"])
-#        # フォルダリストからのデータの読み取り
-#        linklist = {}
-#        with open('folderlist.txt', 'r') as f:
-#            read_data = [x.rstrip() for x in f.readlines()]
-#
-#            for list_item in read_data:
-#                k, v = list_item.split(",")
-#                linklist[k] = v
-#        print(linklist)
-#
-#
-#        # ファイルを閉じる
-#        f.close
-#
-#        # リンクごとにボタンを生成する
-#        for key in linklist:
-#            label = key
-#            folder = linklist[key]
-#
-#            self.label = tk.Button(self)
-#            self.label["text"] = label
-#            self.label.bind("<Enter>", self.change_color_true)
-#            self.label.bind("<Leave>", self.change_color_false)
-#            self.label["command"] = self.run_it(folder)
-#            self.label["anchor"] = "w"
-#            self.label.pack(anchor="w", fill="x")
-#
-#
-#        self.quit = tk.Button(self, text="UPDATE", fg="blue",
-#                              command=self.update_list)
-#        self.quit.pack(anchor="w")
-#
-#
-#        self.quit = tk.Button(self, text="QUIT", fg="red",
-#                              command=self.master.destroy)
-#        self.quit.pack(side="bottom", anchor="w")
-#        
+    def update_folderlist(self):
+        print("start - update_folderlist")
+
+        # self.link_buttons には、現在登録されている全てのボタンウェジットが格納されている
+        current_buttons = self.link_buttons
+
+        for button in current_buttons:
+            print(button)
+            button.destroy()
+
+        self.create_widgets()
 
 
 # 処理開始
